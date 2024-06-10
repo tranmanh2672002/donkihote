6748bd960c539be427d451beb7d230db6399b55d
tranmanh2672002 <tranmanh2672002>
2024-06-10 21:09:48 +0700

    fix env

--- next.config.js
+++ next.config.js
@@ -1,8 +1,16 @@
-// @ts-check
-
 /** @type {import('next').NextConfig} */
+const { MONGODB_URI, NEXT_PUBLIC_CLOUDINARY_API_KEY, NEXT_PUBLIC_CLOUDINARY_API_SECRET, NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME, API_BASEURL } =
+  process.env;
 const nextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  swcMinify: true,
+  env: {
+    MONGODB_URI,
+    NEXT_PUBLIC_CLOUDINARY_API_KEY,
+    NEXT_PUBLIC_CLOUDINARY_API_SECRET,
+    NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
+    API_BASEURL,
+  },
 };
 
 module.exports = nextConfig;
