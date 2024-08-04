1927af75bd998ad8725112eabff526e5c74d1cff
tranmanh2672002 <tranmanh2672002>
2024-08-04 15:04:33 +0700

    dynamic post list

--- pages/product/index.tsx
+++ pages/product/index.tsx
@@ -1,24 +1,12 @@
-import axios from 'axios';
 import BackgroundPattern from '../../components/BackgroundPattern';
 import Footer from '../../components/Footer';
 import Header from '../../components/Header';
-import PostList from '../../components/product/PostList';
 import ScrollToTop from '../../components/ScrollToTop';
-import { IResponseStatus } from '../../interfaces/response.interface';
+import dynamic from 'next/dynamic';
 
-// api
-// export async function getServerSideProps() {
-//   try {
-//     const res2 = await axios.get(`${process.env.API_BASEURL}/post`);
-//     if (res2.data.status === IResponseStatus.success) {
-//       const data = res2.data.data;
-//       return { props: { data } };
-//     }
-//     return { props: { data: [] } };
-//   } catch (error) {
-//     console.log(error);
-//   }
-// }
+const DynamicPostList = dynamic(() => import('../../components/product/PostList'), {
+  ssr: false,
+});
 
 export default function Product() {
   return (
@@ -26,7 +14,7 @@ export default function Product() {
       <BackgroundPattern />
       <Header />
       <div className="relative z-10 max-w-[800px] m-auto mt-[92px] py-4 px-[20px] sm:px-[40px] lg:px-[40]">
-        <PostList />
+        <DynamicPostList />
       </div>
       <Footer />
       <ScrollToTop />
